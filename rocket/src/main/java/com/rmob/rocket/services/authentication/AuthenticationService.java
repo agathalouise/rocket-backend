d257a9c6212ea736c8c082d699e9c1c79cfda542
Agatha Louise <86273896+agathalouise@users.noreply.github.com>
2023-09-22 20:33:41 -0300

    Resolvendo diretorio a mais

--- rocket/src/main/java/com/rmob/rocket/services/authentication/AuthenticationService.java
+++
@@ -1,9 +0,0 @@
-package com.rmob.rocket.services.authentication;
-
-import com.rmob.rocket.entities.authentication.AuthenticateResponse;
-import com.rmob.rocket.entities.authentication.AuthenticateRequest;
-
-public interface AuthenticationService {
-
-	AuthenticateResponse authenticate(AuthenticateRequest request);
-}
